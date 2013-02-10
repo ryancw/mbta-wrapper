3641d4e6ebb26e76e28ca5e0ca1c6564e8a1151d
sicentendu <ryancwilliams9@gmail.com>
2013-02-09 23:40:14 -0500

    CommuterRail added

--- lib/mbta-wrapper.rb
+++ lib/mbta-wrapper.rb
@@ -46,3 +46,29 @@ class Subway
   end
 
 end
+
+class CommuterRail
+  def initialize(line)
+    @line_name = line
+    @line = COMMUTER_RAIL_LINES[line.to_s]
+    if @line.nil?
+      puts 'Error: line name incorrect'
+    end
+  end
+
+  COMMUTER_RAIL_LINES = {'Greenbush' => '1', 'Kingston' => '2', 'Kingston/Plymouth' => '2', 'Plymouth' => '2', 'Middleborough/Lakeville' => '3', 'Middleborough' => '3', 'Lakeville' => '3', 'Fairmount' => '4', 'Providence/Stoughton' => '5', 'Providence' => '5', 'Stoughton' => '5', 'Franklin' => '6', 'Needham' => '7', 'Framingham/Worcester' => '8', 'Framingham' => '8', 'Worcester' => '8', 'Fitchburg' => '9', 'Lowell' => '10', 'Haverhill' => '11', 'Newburyport/Rockport' => '2', 'Newburyport' => '2', 'Rockport' => '2'}
+
+
+  def get_and_parse_json
+    resp = Net::HTTP.get_response(URI.parse("http://developer.mbta.com/lib/RTCR/RailLine_#{@line}.json"))
+    data = JSON.parse(resp.body)
+  end
+
+  # Total active trains in all lines
+  def self.total_active_trains
+  end
+
+  # Active train for given lines
+  def active_trains
+  end
+end
